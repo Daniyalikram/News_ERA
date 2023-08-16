a0651776a3b11daf4401048afd7e066bf631349f
Daniyalikram <142391838+Daniyalikram@users.noreply.github.com>
2023-08-16 05:46:45 -0700

    Update Footer.js

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,25 +4,25 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <>
-      <div className="created-by">Made with ❤️ by Hrishabh Jain</div>
+      <div className="created-by">Made with ❤️ by Muhammad Daniyal Ikram</div>
       <div className="contact">
         <a
           rel="noopener noreferrer"
-          href="https://www.instagram.com/hrishabh.hj/"
+          href="https://wwwhttps://www.instagram.com/dani91429/"
           target="_blank"
         >
           <i class="fa-brands fa-instagram"></i>
         </a>
         <a
           rel="noopener noreferrer"
-          href="https://twitter.com/hrishabhcodes"
+          href="https://twitter.com/Da9iyalIkram"
           target="_blank"
         >
           <i class="fa-brands fa-twitter"></i>
         </a>
         <a
           rel="noopener noreferrer"
-          href="https://github.com/HrishabhCodes"
+          href="https://github.com/Daniyalikram"
           target="_blank"
         >
           <i class="fa-brands fa-github"></i>
